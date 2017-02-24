4301ebdc2775643b275f529141ab856f7f0f2354
bsokat <barissokat@gmail.com>
2017-02-24 17:17:24 +0300

    Personnel Controller added

---
+++ Helpers/Controllers/PersonnelController.cs
@@ -0,0 +1,24 @@
+﻿using Helpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Helpers.Controllers
+{
+    public class PersonnelController : Controller
+    {
+
+        public ActionResult Create()
+        {
+            return View(new Personnel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Personnel newPersonnel)
+        {
+            return View();
+        }
+    }
+}
