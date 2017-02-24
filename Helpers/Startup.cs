169f9435c5f5661e1523b0537274228bcacb5a90
bsokat <barissokat@gmail.com>
2017-02-24 12:41:52 +0300

    Initial commit

---
+++ Helpers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Helpers.Startup))]
+namespace Helpers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
